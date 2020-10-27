7bfca2cec9eb0d261c42834c4eeb96ee50a0e659
Brandon Davis <git@subdavis.com>
2020-10-26 22:08:33 -0400

    Gradients are life

--- components/Toolbar.vue
+++ components/Toolbar.vue
@@ -1,7 +1,15 @@
 <template functional>
   <header class="z-50 absolute lg:fixed w-full flex flex-row justify-between">
-    <div class="hidden lg:block text-white mx-10 my-5 text-md font-mono">
-      <!-- subdavis/subdavis.github.io 791ac8 -->
+    <div class="hidden lg:block text-white mx-8 my-8 text-md font-mono">
+      <a href="https://github.com/subdavis/subdavis.github.io">
+        subdavis/subdavis.github.io
+      </a>
     </div>
   </header>
 </template>
+
+<style scoped>
+a:hover {
+  text-decoration: underline;
+}
+</style>
